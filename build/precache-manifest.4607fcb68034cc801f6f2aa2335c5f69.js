03962d4b4641cae19c335898b52be2ffe7d431b8
miso <marcyx88@gmail.com>
2020-05-19 16:13:44 +0200

    comment build

---
+++ build/precache-manifest.4607fcb68034cc801f6f2aa2335c5f69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c12ea2e0725747b34ca09ebab4044099",
+    "url": "./index.html"
+  },
+  {
+    "revision": "32a92742c8b80a5166bf",
+    "url": "./static/css/main.52abe01a.chunk.css"
+  },
+  {
+    "revision": "8875f00725f03539581d",
+    "url": "./static/js/2.2c0cdf2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2c0cdf2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32a92742c8b80a5166bf",
+    "url": "./static/js/main.e7cb46a6.chunk.js"
+  },
+  {
+    "revision": "3b5176513c78f0fdd135",
+    "url": "./static/js/runtime-main.03defe92.js"
+  }
+]);
